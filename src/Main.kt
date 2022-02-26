fe2647a6731ea815fff648b3b3191f30987ac5ae
CudraMohamed <ncudramohamed@gmail.com>
2022-02-26 20:04:08 +0300

    addFunction2

--- src/Main.kt
+++ src/Main.kt
@@ -6,16 +6,16 @@ fun main() {
     println(chaA.toString() + chaB + chaC)
     var p = pMeter("Cudra", 20)
     println(p)
-    var name = "Mum"
+    var name = "Kudra"
     println("The length of my name is " + name.length)
     var name2 = "Qudra"
     if (name2 == "Mimi") {
-        println("thats me")
+        println("That's me")
     } else {
         print("I don't know who that is")
     }
 }
 fun pMeter(name: String, age: Int): String {
-    var word = "Hi, my name is $name and I am $age years old"
+    var word = "Hi, my name is $name and I am $age years old."
     return word
 }
